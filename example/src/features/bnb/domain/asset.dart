687d0e589edbeeef2ef606ba940d272da790cd7d
inlevkov <io.levk@gmail.com>
2025-11-19 14:16:37 +0100

    code: add BSC (BNB)

---
+++ example/src/features/bnb/domain/asset.dart
@@ -0,0 +1,73 @@
+import 'package:tron_energy_wallet_core/tron_energy_wallet_core.dart';
+
+AppAsset bnbAssetExample({
+  required String address,
+}) => AppAsset(
+  id: 884,
+  balance: 1,
+  hold: 0,
+  address: address,
+  walletId: 4422,
+  childWalletAddress: '',
+  token: const AppToken(
+    id: 331,
+    name: 'BNB',
+    shortName: 'BNB',
+    icon: '',
+    usdPrice: 921,
+    prevPriceDiffPercent: 0.15,
+    contractAddress: '',
+    decimal: 18,
+    precision: 6,
+    blockchain: BlockchainInfo(
+      id: 29,
+      name: 'BNB Smart Chain',
+      shortName: 'BSC',
+      icon: '',
+      isNew: false,
+      tokens: [],
+      appBlockchain: AppBlockchain.bsc,
+      chainId: 97,
+      networkId: 97,
+      supportsEIP1559: false,
+    ),
+    tokenWalletType: TokenWalletType.master,
+    description: 'BNB Smart Chain',
+  ),
+);
+
+AppAsset bscBEP20AssetExample({
+  required String address,
+}) => AppAsset(
+  id: 212,
+  balance: 1,
+  hold: 0,
+  address: address,
+  walletId: 4323,
+  childWalletAddress: '',
+  token: const AppToken(
+    id: 313,
+    name: 'USDT',
+    shortName: 'USDT',
+    icon: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png',
+    usdPrice: 0.9992,
+    prevPriceDiffPercent: 0.09,
+    contractAddress: '0x337610d27c682e347c9cd60bd4b3b107c9d34ddd',
+    decimal: 18,
+    precision: 2,
+    blockchain: BlockchainInfo(
+      id: 29,
+      name: 'BNB Smart Chain',
+      shortName: 'BSC',
+      icon: '',
+      isNew: false,
+      tokens: [],
+      appBlockchain: AppBlockchain.bsc,
+      chainId: 97,
+      networkId: 97,
+      supportsEIP1559: false,
+    ),
+    tokenWalletType: TokenWalletType.child,
+    description: 'USDT',
+  ),
+);
---
+++ example/src/features/bnb/example.dart
@@ -0,0 +1,92 @@
+import 'package:on_chain/ethereum/ethereum.dart';
+import 'package:on_chain/solidity/address/core.dart';
+import 'package:tr_logger/tr_logger.dart';
+import 'package:tron_energy_wallet_core/src/features/networks/ethereum/api/requests/erc_20_balance.dart';
+import 'package:tron_energy_wallet_core/tron_energy_wallet_core.dart';
+
+import '../../common/setup_account.dart';
+import '../../data/repo/local_repo_core_impl.dart';
+import 'domain/asset.dart';
+
+final _rpc = EthereumProvider(
+  EthereumHTTPProvider(
+    'https://data-seed-prebsc-1-s1.binance.org:8545/',
+    '',
+  ),
+);
+
+Future<void> main() async {
+  await setupAccount(
+    mnemonic: 'your-mnemonic',
+  );
+  final localRepo = LocalRepoImpl();
+  final logger = InAppLogger();
+  final ethService = TransactionsServiceEthereumImpl(
+    appBlockchain: AppBlockchain.bsc,
+    localRepo: localRepo,
+    postTransaction: _postTransactionBSC,
+    rpc: _rpc,
+  );
+
+  final walletInfo = await ethService.tryInitializeWalletAndGetInfoOrThrow(
+    masterKey: '',
+  );
+
+  logger.logInfoMessage('bnbExample', 'Main address: ${walletInfo.address}');
+
+  // final asset = bnbAssetExample(
+  //   address: walletInfo.address,
+  // );
+
+  final bal = await _rpc.request(
+    EthereumRequestGetBalance(address: walletInfo.address),
+  );
+  logger.logInfoMessage('bnbExample', 'Balance: $bal');
+
+  // Example asset for ERC20 token transfer
+  final assetBEP20 = bscBEP20AssetExample(address: walletInfo.address);
+  final tokenBalance = await _rpc.request(
+    RPCERC20TokenBalance(
+      assetBEP20.token.contractAddress,
+      SolidityAddress(walletInfo.address),
+    ),
+  );
+  logger.logInfoMessage('bnbExample', 'tokenBalance: $tokenBalance');
+
+  // final feeEstimate = await ethService.tryEstimateFee(
+  //   addressToSend: '0x4204711Fa7FE0a884Ea057987D4E2AC1753181c0',
+  //   asset: asset,
+  //   message: 'hi',
+  // );
+  //
+  final tx = await ethService.createTransactionOrThrow(
+    toAddress: '0x4204711Fa7FE0a884Ea057987D4E2AC1753181c0',
+    amount: 0.01,
+    asset: assetBEP20,
+    masterKey: '',
+    // message: 'hi',
+  );
+  logger.logInfoMessage('bnbExample', 'TX: $tx');
+  final sentTx = await ethService.postTransactionOrThrow(tx: tx);
+  logger.logInfoMessage('bnbExample', 'SENT: $sentTx');
+}
+
+Future<Either<AppExceptionWithCode, TransactionInfoData>> _postTransactionBSC({
+  required AppBlockchain appBlockchain,
+  required String tx,
+  String? txFee,
+}) async {
+  try {
+    final res = await _rpc.request(
+      EthereumRequestSendRawTransaction(transaction: tx),
+    );
+    return Right(
+      TransactionInfoData(
+        txId: res,
+        linkToBlockchain: 'https://testnet.bsctrace.com/tx/$res',
+      ),
+    );
+  } on Exception catch (e) {
+    return Left(AppException(message: e.toString()));
+  }
+}
