4f0be4809474d559691480f20a915c6d92aa8858
inlevkov <io.levk@gmail.com>
2026-02-25 17:09:23 +0100

    code: 1148 eth example extend

--- example/src/features/ethereum/example.dart
+++ example/src/features/ethereum/example.dart
@@ -1,12 +1,16 @@
 import 'package:blockchain_utils/utils/numbers/rational/big_rational.dart';
+import 'package:on_chain/ethereum/src/rpc/methds/get_balance.dart';
 import 'package:on_chain/ethereum/src/rpc/methds/send_raw_transaction.dart';
 import 'package:on_chain/ethereum/src/rpc/provider/provider.dart';
+import 'package:on_chain/solidity/address/core.dart';
 import 'package:tr_logger/tr_logger.dart';
+import 'package:tron_energy_wallet_core/src/features/networks/ethereum/api/requests/erc_20_balance.dart';
 import 'package:tron_energy_wallet_core/tron_energy_wallet_core.dart';
 
 final _rpc = EthereumProvider(
   EthereumHTTPProvider(
-    'https://rpc.ankr.com/eth_sepolia/',
+    // 'https://rpc.ankr.com/eth_sepolia/',
+    'https://ethereum-sepolia-rpc.publicnode.com',
     'your-api-token',
   ),
 );
@@ -34,13 +38,26 @@ Future<void> main() async {
 
   logger.logInfoMessage('ethExample', 'Main address: ${walletInfo.address}');
 
+  final bal = await _rpc.request(
+    EthereumRequestGetBalance(address: walletInfo.address),
+  );
+  logger.logInfoMessage(name, 'Balance: $bal');
+
+  final tokenBalance = await _rpc.request(
+    RPCERC20TokenBalance(
+      _usdcContractAddress,
+      SolidityAddress(walletInfo.address),
+    ),
+  );
+  logger.logInfoMessage(name, 'TokenBalance: $tokenBalance');
   // final feeEstimate = await service.tryEstimateFee(
   //   params: TransferParamsETH(
-  //     to: '0x4204711Fa7FE0a884Ea057987D4E2AC1753181c0',
+  //     to: '0x13606ab8031652FcB18E28676A1a6458806Ffd73',
   //     from: walletInfo.address,
   //     amount: BigRational.parseDecimal('0.001'),
   //     chainId: _chainIdTestNet,
-  //     supportsEIP1559: true, // true | false
+  //     supportsEIP1559: true,
+  //     // true | false
   //     tokenDecimal: 18,
   //     tokenWalletType: TokenWalletType.master,
   //   ),
@@ -51,12 +68,14 @@ Future<void> main() async {
   // USDC Transfer
   // final tx = await service.createTransaction(
   //   params: TransferParamsETH(
-  //     to: '0x4204711Fa7FE0a884Ea057987D4E2AC1753181c0',
+  //     to: '0x13606ab8031652FcB18E28676A1a6458806Ffd73',
   //     from: walletInfo.address,
   //     amount: BigRational.parseDecimal('0.001'),
   //     chainId: _chainIdTestNet,
-  //     supportsEIP1559: true, // true | false
-  //     tokenDecimal: 6, // USDC
+  //     supportsEIP1559: true,
+  //     // true | false
+  //     tokenDecimal: 6,
+  //     // USDC
   //     tokenContractAddress: _usdcContractAddress,
   //     tokenWalletType: TokenWalletType.child,
   //     tokenName: 'USDC',
@@ -67,7 +86,7 @@ Future<void> main() async {
   // ETH transfer
   final tx = await service.createTransaction(
     params: TransferParamsETH(
-      to: '0x4204711Fa7FE0a884Ea057987D4E2AC1753181c0',
+      to: '0x13606ab8031652FcB18E28676A1a6458806Ffd73',
       from: walletInfo.address,
       amount: BigRational.parseDecimal('0.001'),
       chainId: _chainIdTestNet,
